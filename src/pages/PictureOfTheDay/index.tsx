a31ea06e29790b6d9404d141ebd7f345857e3147
Pavel Uvarov <Pavel.Uvarov@kaspersky.com>
2023-10-23 12:45:42 +0300

    add long tasks

--- src/pages/PictureOfTheDay/index.tsx
+++ src/pages/PictureOfTheDay/index.tsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { API_URL_IMAGE_OF_THE_DAY } from "../../constants";
 import { useLoaderData } from "react-router-dom";
 import { PictureOfTheDayResponse } from "../../types";
+import { useEffect, useLayoutEffect } from "react";
+import { myLongTask } from "../../utils/long-tasks";
+import { ReactComponent as GlobeIcon } from "../../img/globe.svg";
 
 const Text = styled.div`
   font-size: 18px;
@@ -26,11 +29,22 @@ const Video = styled.iframe`
 `;
 
 const Title = styled.h1`
+  display: flex;
+  align-items: center;
   font-size: 36px;
   font-weight: bold;
   color: darkcyan;
   margin-bottom: 24px;
 `;
+
+const GlobeIconStyled = styled(GlobeIcon)`
+  fill: darkcyan;
+  width: 28px;
+  margin-left: 12px;
+`;
+
+const PICTURE_URL =
+  "https://images-assets.nasa.gov/image/GSFC_20171208_Archive_e000743/GSFC_20171208_Archive_e000743~orig.png";
 
 export const pictureOfTheDayLoader = () => {
   return fetch(API_URL_IMAGE_OF_THE_DAY);
@@ -39,16 +53,20 @@ export const pictureOfTheDayLoader = () => {
 const PictureOfTheDay = () => {
   const data = useLoaderData() as PictureOfTheDayResponse;
 
+  useLayoutEffect(() => {
+    myLongTask();
+  }, []);
+
   return (
     <Container>
       <>
         <Title>
-          {data.title} <i className="fa fa-globe" aria-hidden="true"></i>
+          {data.title} <GlobeIconStyled />
         </Title>
         {data.media_type === "video" ? (
           <Video frameBorder="0" src={`${data.url}`} />
         ) : (
-          <Image src={data.hdurl} />
+          <Image src={PICTURE_URL} />
         )}
         <Text>{data.explanation}</Text>
       </>
