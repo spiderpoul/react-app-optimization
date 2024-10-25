64f294103eb26f7ad53d2770a63c5e031df1b370
Pavel Uvarov <pavel.uvarov@kaspersky.com>
2024-10-25 12:49:03 +0300

    nextjs image optimization

--- shared/components/PictureOfTheDay/PicOfTheDay.tsx
+++ shared/components/PictureOfTheDay/PicOfTheDay.tsx
@@ -1,17 +1,35 @@
-import { Accordion } from "../Accordion/AccordionList";
-import { accordionData } from "../Accordion/sections";
+import Image from "next/image";
 import styles from "./PicOfTheDay.module.scss";
 
-export const PictureOfTheDay: React.FC<{
+import { Accordion } from "../Accordion/AccordionList";
+import { accordionData } from "../Accordion/sections";
+import { getPlaceholderImage } from "../../../next-js/utils/getPlaceholderImage";
+
+export const PictureOfTheDay = async ({
+  title,
+  explanation,
+  hdurl,
+}: {
   title: string;
   explanation: string;
   hdurl: string;
-}> = ({ title, explanation, hdurl }) => {
+}) => {
+  const imageWithPlaceholder = await getPlaceholderImage(hdurl);
+
   return (
     <div className={styles.container}>
       <>
         <h1 className={styles.title}>{title}</h1>
-        <img className={styles.img} src={hdurl} alt="nebula" />
+        <Image
+          className={styles.img}
+          src={imageWithPlaceholder.src}
+          blurDataURL={imageWithPlaceholder.placeholder}
+          width={700}
+          height={400}
+          quality={80}
+          placeholder="blur"
+          alt="nebula"
+        />
         <div className={styles.text}>{explanation}</div>
       </>
       <div>
