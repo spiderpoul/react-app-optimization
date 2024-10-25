bb1458c6f3bf8938ccfe418ea0ac2699ec1607a4
Pavel Uvarov <pavel.uvarov@kaspersky.com>
2024-10-25 13:53:29 +0300

    nextjs fonts optimization

--- next-js/app/planets/[planet]/page.tsx
+++ next-js/app/planets/[planet]/page.tsx
@@ -3,12 +3,13 @@ import { SEARCH_API_URL } from "../../../../shared/constants";
 
 const planetInfoLoader = ({ params: { planet } }) => {
   return fetch(`${SEARCH_API_URL}${planet}`, {
-    cache: "no-store",
+    next: {
+      revalidate: 60,
+    },
   }).then((res) => res.json());
 };
 
 async function PlanetPage({ params }: { params: { planet: string } }) {
-  await new Promise((r) => setTimeout(r, 3000));
   const data = await planetInfoLoader({ params });
 
   return <CardGrid data={data?.collection} />;
