a4c34270b01d5fee105d84a1fc8ee1c574d6d8ca
Pavel Uvarov <pavel.uvarov@kaspersky.com>
2024-10-25 13:16:16 +0300

    nextjs loading state

--- next-js/app/planets/[planet]/page.tsx
+++ next-js/app/planets/[planet]/page.tsx
@@ -2,7 +2,11 @@ import { CardGrid } from "../../../../shared/components/ImagesGrid/ImagesGrid";
 import { SEARCH_API_URL } from "../../../../shared/constants";
 
 const planetInfoLoader = ({ params: { planet } }) => {
-  return fetch(`${SEARCH_API_URL}${planet}`).then((res) => res.json());
+  return fetch(`${SEARCH_API_URL}${planet}`, {
+    next: {
+      revalidate: 0,
+    },
+  }).then((res) => res.json());
 };
 
 async function PlanetPage({ params }: { params: { planet: string } }) {
