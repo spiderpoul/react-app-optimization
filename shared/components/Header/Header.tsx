cdae2c69bd980c472a9c1f63f70219c470dce556
Pavel Uvarov <pavel.uvarov@kaspersky.com>
2024-10-25 12:49:03 +0300

    nextjs image optimization

--- shared/components/Header/Header.tsx
+++ shared/components/Header/Header.tsx
@@ -1,9 +1,19 @@
 import styles from "./Header.module.scss";
+import Image from "next/image";
+import defaultBg from "../../img/default-bg.png";
 
 export const Header = () => {
   return (
     <div className={styles.headerContainer}>
-      <div className={styles.headerImage} />
+      <Image
+        className={styles.headerImage}
+        src={defaultBg}
+        width={1500}
+        height={500}
+        quality={80}
+        placeholder="blur"
+        alt="nebula"
+      />
     </div>
   );
 };
