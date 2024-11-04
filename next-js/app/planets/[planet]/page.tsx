9edbff4370891a0cea67733a42e5ca06924affb6
Pavel Uvarov <pavel.uvarov@kaspersky.com>
2024-11-04 17:54:59 +0300

    nextjs caching

--- next-js/app/planets/[planet]/page.tsx
+++ next-js/app/planets/[planet]/page.tsx
@@ -2,10 +2,13 @@ import { CardGrid } from "../../../../shared/components/ImagesGrid/ImagesGrid";
 import { SEARCH_API_URL } from "../../../../shared/constants";
 
 const planetInfoLoader = ({ params: { planet } }) => {
-  return fetch(`${SEARCH_API_URL}${planet}`).then((res) => res.json());
+  return fetch(`${SEARCH_API_URL}${planet}`, {
+    cache: "no-store",
+  }).then((res) => res.json());
 };
 
 async function PlanetPage({ params }: { params: { planet: string } }) {
+  await new Promise((r) => setTimeout(r, 3000));
   const data = await planetInfoLoader({ params });
 
   return <CardGrid data={data?.collection} />;
